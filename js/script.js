0a16accb807fd3fca70c787ebef80292a207925c
Ivan Nefagin <dfyz0r@list.ru>
2020-09-03 16:15:07 +0300

    feat(script.js): add timer functionality

---
+++ js/script.js
@@ -0,0 +1,36 @@
+window.addEventListener('DOMContentLoaded', () => {
+  'use strict';
+
+  // Timer
+  const countTimer = deadline => {
+    const timerHours = document.getElementById('timer-hours'),
+      timerMinutes = document.getElementById('timer-minutes'),
+      timerSeconds = document.getElementById('timer-seconds');
+
+    const getTimeRemaining = () => {
+      const dateStop = new Date(deadline).getTime(),
+        dateNow = new Date().getTime();
+
+      const timeRemaining = (dateStop - dateNow) / 1000,
+        seconds = Math.floor(timeRemaining % 60),
+        minutes = Math.floor(timeRemaining / 60 % 60),
+        hours = Math.floor(timeRemaining / 3600);
+
+      return {timeRemaining, hours, minutes, seconds};
+    };
+
+    const updateTimer = () => {
+      const timer = getTimeRemaining();
+
+      timerHours.textContent = timer.hours;
+      timerMinutes.textContent = timer.minutes;
+      timerSeconds.textContent = timer.seconds;
+
+      if (timer.timeRemaining > 0) setTimeout(updateTimer, 1000);
+    }
+
+    updateTimer();
+  };
+
+  countTimer('03 september 2020 15:58');
+});
