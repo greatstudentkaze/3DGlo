d115710be9cf2c514868042fe7c97cfd6dae5c7c
gsk <dfyz0r@list.ru>
2020-09-05 13:17:42 +0300

    chore(script.js): remove jshint errors, change curly rule value to false

--- js/script.js
+++ js/script.js
@@ -21,6 +21,7 @@ window.addEventListener('DOMContentLoaded', () => {
       return {timeRemaining, hours, minutes, seconds};
     };
 
+    let timerInterval;
     const updateTimer = () => {
       const timer = getTimeRemaining();
 
@@ -34,10 +35,10 @@ window.addEventListener('DOMContentLoaded', () => {
         timerMinutes.textContent = '00';
         timerSeconds.textContent = '00';
       }
-    }
+    };
 
-    const timerInterval = setInterval(updateTimer, 1000);
     updateTimer();
+    timerInterval = setInterval(updateTimer, 1000);
   };
 
   countTimer('24 september 2020 00:00');
@@ -104,17 +105,15 @@ window.addEventListener('DOMContentLoaded', () => {
 
       if (document.documentElement.clientWidth >= 768) {
         popupContent.style.top = '-100%';
-        function makeEaseOut(timing) {
-          return (timeFraction) => 1 - timing(1 - timeFraction);
-        }
+        const makeEaseOut = timing => (timeFraction) => 1 - timing(1 - timeFraction);
 
-        function bounce(timeFraction) {
+        const bounce = timeFraction => {
           for (let a = 0, b = 1, result; 1; a += b, b /= 2) {
             if (timeFraction >= (7 - 4 * a) / 11) {
-              return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2)
+              return -Math.pow((11 - 6 * a - 11 * timeFraction) / 4, 2) + Math.pow(b, 2);
             }
           }
-        }
+        };
 
         animate({
           duration: 800,
