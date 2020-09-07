1fcc2cf5297928e80a8ac655cd2100ab0f869367
Ivan Nefagin <dfyz0r@list.ru>
2020-09-07 17:43:44 +0300

    feat(script.js): add .command__photo change on hover

--- js/script.js
+++ js/script.js
@@ -288,4 +288,26 @@ window.addEventListener('DOMContentLoaded', () => {
   };
 
   slider();
+
+  // Image hover
+  const hoverOverImage = () => {
+    const imageRow = document.querySelector('.command .row');
+
+    let imageSrc;
+
+    imageRow.addEventListener('mouseover', evt => {
+      if (!evt.target.classList.contains('command__photo')) return;
+
+      imageSrc = evt.target.src;
+      evt.target.src = evt.target.dataset.img;
+    });
+
+    imageRow.addEventListener('mouseout', evt => {
+      if (!evt.target.classList.contains('command__photo')) return;
+
+      evt.target.src = imageSrc;
+    });
+  };
+
+  hoverOverImage();
 });
