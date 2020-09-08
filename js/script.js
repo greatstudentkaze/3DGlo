70424f80ae06600073d9d670b8c19420bc810fe5
Ivan Nefagin <dfyz0r@list.ru>
2020-09-08 09:09:16 +0300

    feat(script.js): add calculator functionality

--- js/script.js
+++ js/script.js
@@ -311,15 +311,50 @@ window.addEventListener('DOMContentLoaded', () => {
 
   hoverOverImage();
 
-  // Calculator Validation
-  const calcValidation = () => {
-    const inputs = document.querySelectorAll('input.calc-item');
+  // Calculator
+  const calculator = (price = 100) => {
+    const calcBlock = document.querySelector('.calc-block'),
+      inputType = calcBlock.querySelector('.calc-type'),
+      inputArea = calcBlock.querySelector('.calc-square'),
+      inputAmount = calcBlock.querySelector('.calc-count'),
+      inputDays = calcBlock.querySelector('.calc-day'),
+      totalCost = document.getElementById('total');
 
-    inputs.forEach(elem =>
-      elem.addEventListener('input', evt => {
-        evt.target.value = evt.target.value.replace(/\D/g, '');
-      }));
+    const calcCost = () => {
+      let total = 0,
+        amountValue = 1,
+        daysValue = 1;
+
+      const typeValue = inputType.options[inputType.selectedIndex].value,
+        areaValue = +inputArea.value;
+
+      if (inputAmount.value > 1) {
+        amountValue += (inputAmount.value - 1) / 10;
+      }
+
+      if (inputDays.value && inputDays.value < 5) {
+        daysValue *= 2;
+      } else if (inputDays.value && inputDays.value < 10) {
+        daysValue *= 1.5;
+      }
+
+      if (typeValue && areaValue) {
+        total = price * typeValue * areaValue * amountValue * daysValue;
+      }
+
+      totalCost.textContent = total;
+    };
+
+    calcBlock.addEventListener('input', evt => {
+      const target = evt.target;
+
+      if (target.matches('select') || target.matches('input')) {
+        if (target.matches('input')) target.value = target.value.replace(/\D/g, '');
+        calcCost();
+      }
+    });
+
   };
 
-  calcValidation();
+  calculator();
 });
