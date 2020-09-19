5f11df10c6e5bb93bc0fffa65707566dbbe745aa
Ivan Nefagin <dfyz0r@list.ru>
2020-09-19 08:58:39 +0300

    refactor(tabs.js): change for to forEach

--- src/modules/tabs.js
+++ src/modules/tabs.js
@@ -4,22 +4,22 @@ const tabs = () => {
     tabsContent = document.querySelectorAll('.service-tab');
 
   const toggleTabContent = index => {
-    for (let i = 0; i < tabsContent.length; i++) {
+    tabsContent.forEach((tabContent, i) => {
       if (index === i) {
         tabs[i].classList.add('active');
-        tabsContent[i].classList.remove('d-none');
+        tabContent.classList.remove('d-none');
       } else {
         tabs[i].classList.remove('active');
-        tabsContent[i].classList.add('d-none');
+        tabContent.classList.add('d-none');
       }
-    }
+    });
   };
 
   tabHeader.addEventListener('click', evt => {
     const target = evt.target.closest('.service-header-tab');
     if (target) {
-      tabs.forEach((item, index) => {
-        if (item === target) {
+      tabs.forEach((tab, index) => {
+        if (tab === target) {
           toggleTabContent(index);
         }
       });
